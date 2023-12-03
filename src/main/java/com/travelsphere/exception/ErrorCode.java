27ef6793ce309754b94f52c7092ea0e803ff081a
HyunsooZo <bzhs1992@icloud.com>
2023-12-04 03:32:40 +0900

    Feat: 로그인 관련 예외 메세지 추가

--- src/main/java/com/travelsphere/exception/ErrorCode.java
+++ src/main/java/com/travelsphere/exception/ErrorCode.java
@@ -16,7 +16,9 @@ public enum ErrorCode {
 
     //user
     USER_INFO_NOT_FOUND(NOT_FOUND, "존재하지 않는 사용자입니다."),
-    EXISTING_USER(BAD_REQUEST, "이미 존재하는 사용자입니다.");
+    EXISTING_USER(BAD_REQUEST, "이미 존재하는 사용자입니다."),
+    WRONG_PASSWORD(BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    USER_STATUS_NOT_ACTIVE(BAD_REQUEST, "사용자 상태가 활성화되어 있지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
