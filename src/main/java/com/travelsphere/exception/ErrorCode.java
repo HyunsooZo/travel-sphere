e314870648bb2059bb3d13f47a59c0de3988e8d2
HyunsooZo <bzhs1992@icloud.com>
2023-12-15 00:59:31 +0900

    Feat: 지출내역 수정관련 예외 추가

--- src/main/java/com/travelsphere/exception/ErrorCode.java
+++ src/main/java/com/travelsphere/exception/ErrorCode.java
@@ -19,9 +19,13 @@ public enum ErrorCode {
     EXISTING_USER(BAD_REQUEST, "이미 존재하는 사용자입니다."),
     WRONG_PASSWORD(BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     USER_STATUS_NOT_ACTIVE(BAD_REQUEST, "사용자 상태가 활성화되어 있지 않습니다."),
-    WEATHER_NOT_FOUND(NOT_FOUND, "지원하지 않는 도시입니다. 도시이름을 확인해주세요.");
+    WEATHER_NOT_FOUND(NOT_FOUND, "지원하지 않는 도시입니다. 도시이름을 확인해주세요."),
+
+    //expense
+    EXPENSE_NOT_FOUND(NOT_FOUND, "존재하지 않는 지출입니다."),
+    NOT_MY_EXPENSE(BAD_REQUEST, "본인의 지출만 수정/삭제할 수 있습니다.");
 
     private final HttpStatus status;
     private final String message;
 
-}
+    }
